cf87d7f269dc6a18edba95890978f5aa0a880440
krzyty <1212471339@st.usst.edu.cn>
2016-12-18 01:27:06 +0800

    Dao

--- Hospital/src/team/sqjj/hospital/dao/DrugDAO.java
+++ Hospital/src/team/sqjj/hospital/dao/DrugDAO.java
@@ -2,10 +2,9 @@ package team.sqjj.hospital.dao;
 
 import team.sqjj.hospital.model.Drug;
 
-public interface DrugDAO {
-public void AddDrug(String drugid,String drugname,double price,String unit,int amount);
-public void UpdateDrug(String drugid,String drugname,double price,String unit,int amount);
-public void DelDrug(String drugid);
-public Drug FindByID(String drugid);
-
+public interface DrugDao {
+public int addDrug(String drugId,String drugName,double price,String unit,int amount);
+public int updateDrug(String drugId,String drugName,double price,String unit,int amount);
+public int delDrug(String drugId);
+public Drug findByID(String drugId);
 }
--- Hospital/src/team/sqjj/hospital/dao/OrderDAO.java
+++ Hospital/src/team/sqjj/hospital/dao/OrderDAO.java
@@ -1,17 +1,12 @@
 package team.sqjj.hospital.dao;
 
 import java.util.List;
-
 import team.sqjj.hospital.model.Order;
 
-public interface OrderDAO {
-public void AddOrder(String patientid,String doctorid,String department,String drugid,int quantity);
-
-public void PayOrder(String orderid);
-public List<Order> GetByPatientID (String patientid);
-public double GetMoneyByDoctor(String doctorid);
-public double GetMoneyByDepartment(String department);
-
-
-
+public interface OrderDao {
+public int addOrder(int drugId,String patientId,String id,String doctorName,String department,String drugName,int quantity,String unit,int price,String code,int isPaid);//业务层中先要查询出完全的药品信息，在此添加
+public int payOrder(String orderId);
+public List<Order> getByPatientID (String patientId);
+public List<Order> getByDoctor(String id);//在业务层中用此方法来获取某医生处的收入 id为医生id
+public List<Order> getByDepartment(String department);//在业务层中用此方法来获取某处的收入
 }
