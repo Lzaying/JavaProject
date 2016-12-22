b692237bfb0e5c1f8b82524ed0384a4cf19a1031
Lzaying <752900213@qq.com>
2016-12-22 17:01:29 +0800

    两个关于DAO层的大小写错误

--- Hospital/src/team/sqjj/hospital/dao/DrugDAO.java
+++ Hospital/src/team/sqjj/hospital/dao/DrugDAO.java
@@ -2,9 +2,12 @@ package team.sqjj.hospital.dao;
 
 import team.sqjj.hospital.model.Drug;
 
-public interface DrugDao {
-public int addDrug(String drugId,String drugName,double price,String unit,int amount);
-public int updateDrug(String drugId,String drugName,double price,String unit,int amount);
-public int delDrug(String drugId);
-public Drug findByID(String drugId);
+public interface DrugDAO {
+	public int addDrug(String drugId, String drugName, double price, String unit, int amount);
+
+	public int updateDrug(String drugId, String drugName, double price, String unit, int amount);
+
+	public int delDrug(String drugId);
+
+	public Drug findByID(String drugId);
 }
