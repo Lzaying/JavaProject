d9464efba348e21cfb5efecaff1c2ddf21a9aec5
jinjintang <1002682830@qq.com>
2016-12-22 20:48:18 +0800

    there is no mistake

--- Hospital/src/team/sqjj/hospital/DaoFactory/AppointmentDaoFactory.java
+++ Hospital/src/team/sqjj/hospital/DaoFactory/AppointmentDaoFactory.java
@@ -1,5 +1,8 @@
 package team.sqjj.hospital.DaoFactory;
 
+import team.sqjj.hospital.DaoImpl.AppointmentDaoImpl;
+import team.sqjj.hospital.dao.AppointmentDao;
+
 public class AppointmentDaoFactory {
 	public static AppointmentDao getInstance(){
 		return new AppointmentDaoImpl();
--- Hospital/src/team/sqjj/hospital/DaoFactory/DoctorDaoFactory.java
+++ Hospital/src/team/sqjj/hospital/DaoFactory/DoctorDaoFactory.java
@@ -1,5 +1,8 @@
 package team.sqjj.hospital.DaoFactory;
 
+import team.sqjj.hospital.DaoImpl.DoctorDaoImpl;
+import team.sqjj.hospital.dao.DoctorDao;
+
 public class DoctorDaoFactory {
 	public static DoctorDao getInstance(){
 		return new DoctorDaoImpl();
