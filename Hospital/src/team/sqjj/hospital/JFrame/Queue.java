5ddfa2ceb8f0e3f20477eb65fc1df57cacaf91c4
jinjintang <1002682830@qq.com>
2016-12-25 19:08:21 +0800

    paidui

---
+++ Hospital/src/team/sqjj/hospital/JFrame/Queue.java
@@ -0,0 +1,14 @@
+package team.sqjj.hospital.JFrame;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import team.sqjj.hospital.model.Patient;
+
+public class Queue {
+	public static Map<String,List<Patient>> qlist=new HashMap<String,List<Patient>> ();
+public static Map<String,List<Patient>> qlist1=new HashMap<String,List<Patient>> ();
+public static Map<String,List<Patient>> qlist2=new HashMap<String,List<Patient>> ();
+
+}
--- Hospital/src/team/sqjj/hospital/Util/StationConstant.java
+++
@@ -1,9 +0,0 @@
-
-package team.sqjj.hospital.Util;
-
-public class StationConstant {
-    /**
-     * 此处站点分层2个字符串 因为超过了字符串的存储长度(65535) 中国的火车站点还真是多
-     */
-    // 所有站点常量类
-    public final static String stationString1 = "@bjb|北京北|VAP|beijingbei|bjb|0@bjd|北京东|BOP|beijingdong|bjd|1@bji|北京|BJP|beijing|bj|2@bjn|北京南|VNP|beijingnan|bjn|3@bjx|北京西|BXP|beijingxi|bjx|4@cqb|重庆北|CUW|chongqingbei|zqb|5@cqi|重庆|CQW|chongqing|zq|6@cqn|重庆南|CRW|chongqingnan|zqn|7@sha|上海|SHH|shanghai|sh|8@shn|上海南|SNH|shanghainan|shn|9@shq|上海虹桥|AOH|shanghaihongqiao|shhq|10@shx|上海西|SXH|shanghaixi|shx|11@tjb|天津北|TBP|tianjinbei|tjb|12@tji|天津|TJP|tianjin|tj|13@tjn|天津南|TIP|tianjinnan|tjn|14@tjx|天津西|TXP|tianjinxi|tjx|15@cch|长春|CCT|changchun|cc|16@ccn|长春南|CET|changchunnan|ccn|17@ccx|长春西|CRT|changchunxi|ccx|18@cdd|成都东|ICW|chengdudong|cdd|19@cdn|成都南|CNW|chengdunan|cdn|20@cdu|成都|CDW|chengdu|cd|21@csh|长沙|CSQ|changsha|cs|22@csn|长沙南|CWQ|changshanan|csn|23@fzh|福州|FZS|fuzhou|fz|24@fzn|福州南|FYS|fuzhounan|fzn|25@gya|贵阳|GIW|guiyang|gy|26@gzb|广州北|GBQ|guangzhoubei|gzb|27@gzd|广州东|GGQ|guangzhoudong|gzd|28@gzh|广州|GZQ|guangzhou|gz|29@gzn|广州南|IZQ|guangzhounan|gzn|30@heb|哈尔滨|HBB|haerbin|heb|31@hed|哈尔滨东|VBB|harbindong|hebd|32@hex|哈尔滨西|VAB|haerbinxi|hebx|33@hfe|合肥|HFH|hefei|hf|34@hfx|合肥西|HTH|hefeixi|hfx|35@hhd|呼和浩特东|NDC|huhehaotedong|hhhtd|36@hht|呼和浩特|HHC|hohhot|hhht|37@hkd|海口东|HMQ|haikoudong|hkd|38";}
