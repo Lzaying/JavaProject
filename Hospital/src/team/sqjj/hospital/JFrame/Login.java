6c36d1f2aca01766ed65e2702a0a73e9479bfb2d
hillary <15901896207@163.com>
2016-12-26 13:05:43 +0800

    login
    1

--- Hospital/src/team/sqjj/hospital/JFrame/Login.java
+++ Hospital/src/team/sqjj/hospital/JFrame/Login.java
@@ -2,11 +2,11 @@ package team.sqjj.hospital.JFrame;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.BorderLayout;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.JButton;
 import javax.swing.JRadioButton;
@@ -30,66 +30,50 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 import java.awt.event.FocusAdapter;
 import java.awt.event.FocusEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class Login extends JFrame implements ActionListener{
 	private JTextField textField;
 	private JTextField textField_1;
 	private JButton doc;
-	private final Action action = new SwingAction();
 	public Login() {
 		super("登录");
 			
 		getContentPane().setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("用户名");
-		lblNewLabel.setBounds(41, 52, 54, 15);
+		lblNewLabel.setBounds(54, 52, 54, 15);
 		getContentPane().add(lblNewLabel);
 		
 		JLabel lblNewLabel_1 = new JLabel("密码");
-		lblNewLabel_1.setBounds(41, 77, 54, 15);
+		lblNewLabel_1.setBounds(54, 92, 54, 15);
 		getContentPane().add(lblNewLabel_1);
 		
 		textField = new JTextField();
-		textField.addFocusListener(new FocusAdapter() {
-			@Override
-			public void focusLost(FocusEvent e) {
-				textField_1.setText("密码不用输了");
-			}
-		});
-		textField.setBounds(105, 49, 143, 21);
+		textField.setBounds(118, 49, 143, 21);
 		getContentPane().add(textField);
 		textField.setColumns(10);
 		
 		textField_1 = new JTextField();
-		textField_1.setBounds(105, 74, 143, 21);
+		textField_1.setBounds(118, 89, 143, 21);
 		getContentPane().add(textField_1);
 		textField_1.setColumns(10);
 		
 		doc = new JButton("登录");
-		doc.setBounds(31, 156, 93, 23);
+		doc.setBounds(52, 156, 93, 23);
 		getContentPane().add(doc);
 		doc.addActionListener(this);
 		
 		JButton btnNewButton_1 = new JButton("退出");
-		btnNewButton_1.setAction(action);
-		btnNewButton_1.setBounds(155, 156, 93, 23);
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dispose();
+			}
+		});
+		btnNewButton_1.setBounds(168, 156, 93, 23);
 		getContentPane().add(btnNewButton_1);
-		
-		JRadioButton rdbtnNewRadioButton = new JRadioButton("\u533B\u751F");
-		rdbtnNewRadioButton.setBounds(19, 115, 63, 23);
-		getContentPane().add(rdbtnNewRadioButton);
-		
-		JRadioButton rdbtnNewRadioButton_1 = new JRadioButton("\u9662\u957F");
-		rdbtnNewRadioButton_1.setBounds(96, 115, 63, 23);
-		getContentPane().add(rdbtnNewRadioButton_1);
-		
-		JRadioButton rdbtnNewRadioButton_2 = new JRadioButton("\u836F\u5E08");
-		rdbtnNewRadioButton_2.setBounds(168, 115, 63, 23);
-		getContentPane().add(rdbtnNewRadioButton_2);
-		
-		JRadioButton rdbtnNewRadioButton_3 = new JRadioButton("\u6536\u8D39");
-		rdbtnNewRadioButton_3.setBounds(233, 115, 63, 23);
-		getContentPane().add(rdbtnNewRadioButton_3);
+
 		
 		JLabel lblNewLabel_2 = new JLabel("医院管理系统");
 		lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
@@ -102,36 +86,40 @@ public class Login extends JFrame implements ActionListener{
 			
 	}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		new Login();
-	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub		
 		if(e.getSource()==doc){
-		String username=textField.getText();
-		String password=textField_1.getText();
-			User user=	UserDaoFactory.getInstance().getById(username);
-			if(user!=null&&user.getPassword()==password){
-				if(user.getRole()==0){new Client_Admin();}
-				else if(user.getRole()==1){new Client_Register();}
-				else if(user.getRole()==2){new Client_Doctor(username);}
-				else if(user.getRole()==3){new Client_Dean();}
-				else if(user.getRole()==4){new Client_DrugStore();}
+
+
+			if(textField.getText().length()==0){
+				JOptionPane.showMessageDialog(this, "用户名不能为空！");
 			}
-			else{
-				
+			else if(textField_1.getText().length()==0){
+				JOptionPane.showMessageDialog(this, "密码不能为空！");
+			}else{
+				String username=textField.getText();
+				String password=textField_1.getText();
+				User user=	UserDaoFactory.getInstance().getById(username);
+				if(user!=null&&user.getPassword()==password){
+					if(user.getRole()==0){new Client_Admin();}
+					else if(user.getRole()==1){new Client_Register();}
+					else if(user.getRole()==2){new Client_Doctor(username);}
+					else if(user.getRole()==3){new Client_Dean();}
+					else if(user.getRole()==4){new Client_DrugStore();}
+				}
+				else{
+					JOptionPane.showMessageDialog(this, "用户名或密码错误！请重试");
+				}
 			}
-	}}
-	private class SwingAction extends AbstractAction {
-		public SwingAction() {
-			putValue(NAME, "退出");//逻辑问题
-			putValue(SHORT_DESCRIPTION, "Some short description");
 		}
-		public void actionPerformed(ActionEvent e) {
-			dispose();
-		}
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new Login();
 	}
 }
